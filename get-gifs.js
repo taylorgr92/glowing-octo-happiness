e7495237b5ea0cdc0599eb0a41dcfb799acae3a3
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-13 14:06:07 -0500

    declaring gif variables and api key

---
+++ get-gifs.js
@@ -0,0 +1,10 @@
+
+//assign variables
+var gifSectEl=document.getElementById("#gifSect");
+var btnSearchEl=document.getElementById("#btnSearch")
+
+//giphy API key
+var APIKEY = "k0CkMRxLxYYn0D6z8cZC35N8woAyytgJ";
+
+//event listener to load page 
+document.addEventListener("DOMContentLoaded", initGifs);
