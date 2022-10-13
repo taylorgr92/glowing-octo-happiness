41686600357d90d97c1093a9e5c73e02b80d5cfe
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-13 14:12:23 -0500

    fetch call for json data

--- get-gifs.js
+++ get-gifs.js
@@ -1,10 +1,28 @@
-
-//assign variables
-var gifSectEl=document.getElementById("#gifSect");
-var btnSearchEl=document.getElementById("#btnSearch")
+//assign dom manipulation variables
+var gifSectEl = document.getElementById("#gifSect");
+var btnSearchEl = document.getElementById("#btnSearch");
 
 //giphy API key
 var APIKEY = "k0CkMRxLxYYn0D6z8cZC35N8woAyytgJ";
 
-//event listener to load page 
+//event listener to load page
 document.addEventListener("DOMContentLoaded", initGifs);
+function initGifs() {
+  document.getElementById("btnSearch").addEventListener("click", (ev) => {
+    ev.preventDefault(); //to stop the page reload
+
+    var url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=10&q=categorymovie`; //limits search results to the movie category with a limit of 10
+    var gifStr = document.getElementById("search").value.trim();
+    url = url.concat(gifStr);
+    getMovieGifs(url); //
+  });
+}
+
+///fetch request for gif call
+
+var fetchRes = function (url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((content) => {
+        console.log(content.data[0]);
+      })}
