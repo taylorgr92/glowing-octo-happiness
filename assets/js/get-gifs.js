718889d5bbda15b1b85fe66f87fc12a4cf4c513b
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-13 15:31:17 -0500

    adjust gif container padding

--- assets/js/get-gifs.js
+++ assets/js/get-gifs.js
@@ -11,7 +11,7 @@ function initGifs() {
   document.getElementById("btnSearch").addEventListener("click", (ev) => {
     ev.preventDefault(); //to stop the page reload
 
-    var url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=10&q=categorymovie`; //limits search results to the movie category with a limit of 10
+    var url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=20&q=categorymovie`; //limits search results to the movie category with a limit of 10
     var gifStr = document.getElementById("search").value.trim();
     url = url.concat(gifStr);
     getMovieGifs(url); //
