6dffc18dc3a59c97803d7cd5341c4b299856a58c
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-22 21:48:40 -0500

    edited comments

--- assets/js/get-gifs.js
+++ assets/js/get-gifs.js
@@ -11,9 +11,10 @@ document.addEventListener("DOMContentLoaded", initGifs);
 function initGifs() {
   document.getElementById("btnSearch").addEventListener("click", (ev) => {
     ev.preventDefault(); //to stop the page reload
-
+    
+  //limits search results to the movie category with a limit of 15
     var url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=15&q=categorymovie `;
-    //limits search results to the movie category with a limit of 10
+  
     var gifStr = document.getElementById("search").value.trim();
     console.log(gifStr);
     url = url.concat(gifStr);
