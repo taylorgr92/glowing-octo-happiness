c17c24c96631886819baa31257c057ccbcf4789b
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-11-16 17:53:34 -0600

    fixed comments

--- assets/js/get-gifs.js
+++ assets/js/get-gifs.js
@@ -1,28 +1,25 @@
-//assign dom manipulation variables
-
+//Assign DOM manipulation variables
 var btnSearchEl = document.getElementById("#btnSearch");
 var gifSectEl = document.querySelector(".gifSect");
 
-// giphy API key
-
+// Giphy API key
 var APIKEY = "qAY2xUeCahTZp2g4nhu5pjwMQPcOQINZ";
-//event listener to load page
+//Event listener to load page
 document.addEventListener("DOMContentLoaded", initGifs);
 function initGifs() {
   document.getElementById("btnSearch").addEventListener("click", (ev) => {
     ev.preventDefault(); //to stop the page reload
-    
-  //limits search results to the movie category with a limit of 15
+
+    //Limits search results to the movie category with a limit of 15
     var url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=15&q=categorymovie `;
-  
+
     var gifStr = document.getElementById("search").value.trim();
     console.log(gifStr);
     url = url.concat(gifStr);
     getMovieGifs(url); //
   });
 }
-///fetch request for gif
-
+///Fetch request for gif
 var getMovieGifs = function (url) {
   gifSectEl.textContent = "";
 
@@ -30,7 +27,7 @@ var getMovieGifs = function (url) {
     .then((response) => response.json())
     .then((content) => {
       console.log(content.data[0]);
-      //looping through gifs to display each
+      //Looping through gifs to display each
       for (var i = 0; i < content.data.length; i++) {
         var video = document.createElement("video");
         //looping property allows gif to loop for 15 seconds
@@ -39,12 +36,12 @@ var getMovieGifs = function (url) {
         video.style.height = "200px";
         video.style.width = "200px";
         video.style.padding = "20px";
-        //setting autoplay attribute so gif plays on page load
+        //Setting autoplay attribute so gif plays on page load
         video.setAttribute("autoplay", "true");
 
         console.log(video);
 
-        //appends gif image to section
+        //Appends gif image to section
         gifSectEl.append(video);
       }
     });
