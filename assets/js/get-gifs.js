a2d917ef6fe70c1848e991056d820eb9c9a27c0f
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2023-02-04 12:44:07 -0600

    refactor js file to reactjs

--- assets/js/get-gifs.js
+++ assets/js/get-gifs.js
@@ -1,48 +1,38 @@
-//Assign DOM manipulation variables
-var btnSearchEl = document.getElementById("#btnSearch");
-var gifSectEl = document.querySelector(".gifSect");
+import React, { useState, useEffect } from "react";
 
-// Giphy API key
-var APIKEY = "qAY2xUeCahTZp2g4nhu5pjwMQPcOQINZ";
-//Event listener to load page
-document.addEventListener("DOMContentLoaded", initGifs);
-function initGifs() {
-  document.getElementById("btnSearch").addEventListener("click", (ev) => {
-    ev.preventDefault(); //to stop the page reload
+const APIKEY = "qAY2xUeCahTZp2g4nhu5pjwMQPcOQINZ";
 
-    //Limits search results to the movie category with a limit of 15
-    var url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=15&q=categorymovie `;
+const GifSearch = () => {
+  const [gifStr, setGifStr] = useState("");
+  const [gifs, setGifs] = useState([]);
 
-    var gifStr = document.getElementById("search").value.trim();
-    console.log(gifStr);
-    url = url.concat(gifStr);
-    getMovieGifs(url); //
-  });
-}
-///Fetch request for gif
-var getMovieGifs = function (url) {
-  gifSectEl.textContent = "";
+  const handleSearch = (event) => {
+    event.preventDefault();
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((content) => {
-      console.log(content.data[0]);
-      //Looping through gifs to display each
-      for (var i = 0; i < content.data.length; i++) {
-        var video = document.createElement("video");
-        //looping property allows gif to loop for 15 seconds
-        video.src = content.data[i].images.looping.mp4;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=15&q=category:movie ${gifStr}`;
 
-        video.style.height = "200px";
-        video.style.width = "200px";
-        video.style.padding = "20px";
-        //Setting autoplay attribute so gif plays on page load
-        video.setAttribute("autoplay", "true");
+    fetch(url)
+      .then((response) => response.json())
+      .then((content) => {
+        setGifs(content.data);
+      });
+  };
 
-        console.log(video);
-
-        //Appends gif image to section
-        gifSectEl.append(video);
-      }
-    });
+  return (
+    <div>
+      <form>
+        <input type="text" value={gifStr} onChange={(event) => setGifStr(event.target.value)} />
+        <button id="btnSearch" onClick={handleSearch}>Search</button>
+      </form>
+      <section className="gifSect">
+        {gifs.map((gif) => (
+          <video key={gif.id} height="200px" width="200px" padding="20px" autoPlay>
+            <source src={gif.images.looping.mp4} />
+          </video>
+        ))}
+      </section>
+    </div>
+  );
 };
+
+export default GifSearch;
